cf12d4c2450deff38758f85566497c14c907577a
abc <brendandrachler@gmail.com>
2021-07-19 18:06:43 -0400

    Adding comments and changing the user on my local machine so commits look like they are coming from the right person.

--- produce_simulated_dataset.py
+++ produce_simulated_dataset.py
@@ -23,17 +23,17 @@ from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
 
 import argparse
 
-#Initialize the parser to accept user inputs
+## Initialize the parser to accept user inputs
 parser = argparse.ArgumentParser(description = "Initiate data sets with stochastic GWB injections with given numpy seed (realization) and amplitudes")
 
-#Required arguments:
+## Required arguments:
 parser.add_argument("-timpath", required = True, help = "Path to base directory holding timfiles")
 parser.add_argument("-parpath", required = True, help = "Path to base directory containing all parfiles")
 parser.add_argument("-timsavepath", required = True, help = "Path to directory where we will save the new timfiles")
 
 
 
-#Optional arguments:
+## Optional arguments:
 parser.add_argument("--datacut_length", dest='datacut_length', default=7.0, help="The minimum observing baseline a pulsar must meet in order to be included in the simulation.", type=float)
 parser.add_argument("--read_pickle", dest = 'read_pickle', action = 'store', default = True, help = "Flag to read dataset as pickle to save i/o time; Default: True")
 parser.add_argument("--ephemeris", dest = 'ephem', help = "Choose solar system ephemeris for loading in pulsars; Default: DE438", choices = ['DE430', 'DE435', 'DE436', 'DE438', 'BayesEphem'], default = 'DE4368')
@@ -46,9 +46,10 @@ parser.add_argument("--fdm_phase_p", dest = 'fdm_phase_p', help = "The fuzzy dar
 parser.add_argument("--fdm_phase_e", dest = 'fdm_phase_e', help = "The fuzzy dark matter earth term phase to inject; Default: None", default=None, type=float)
 parser.add_argument("--GWB_A", dest = 'GWB_A', help = "The GWB amplitude to inject; Default: None", default=None, type=float)
 
-#Load the arguments:
+## Load the arguments:
 args = parser.parse_args()
 
+## Begin writing all arguments to an output file for record keeping
 output_params = open(args.timsavepath + "simulation_params.txt","a")
 output_params.write(str(args) + '\n')
 
@@ -61,15 +62,18 @@ if args.inject_fdm:
                      " The values you've provided are fdm_A, fdm_f, fdm_phase_p, fdm_phase_e = " + str(args.fdm_A) + ', ' + str(args.fdm_f) + ', ' \
                      + str(args.fdm_phase_p) + ', ' + str(args.fdm_phase_e))
 
+## Check if the user will be injecting a gravitational wave background (GWB)
 if args.inject_GWB:
 
+    ## If they do, make sure they specified the GWB amplitude
     if args.GWB_A is None:
         raise ValueError("You have decided to inject a gravitational background signal but did not specify the injected amplitude.")
 
+## Check if the user has a pickle file containing the par and tim files.
 if args.read_pickle is True:
-    print(args.read_pickle)
-    psrcut = pickle.load(open("psrs.p", "rb"))
+    psrcut = pickle.load(open(args.parpath + "psrs.p", "rb"))
 
+    ## Get the names of the pulsars that are in pickled dataset.
     psrcut_parfiles = []
     for psr in psrcut:
         for parfile in parfiles:
@@ -82,21 +86,25 @@ if args.read_pickle is True:
             if psr.name in timfile:
                 psrcut_timfiles.append(timfile)
 
-
+## If not, get the par and tim from their usual txt format. 
 else:
+    ## Get all of the par/tim files sorted.
     parfiles = sorted(glob.glob(args.parpath + '*.par'))
     timfiles = sorted(glob.glob(args.timpath + '*.tim'))
 
+    ## Get the tempo2 version of J1713
     for parfile in parfiles:
         if 'J1713' in parfile and 't2' not in parfile:
             parfiles.remove(parfile)
 
+    ## Zip the par/tim files for easy access.
     psrs = []
     for p, t in zip(parfiles, timfiles):
         psrname = parfiles[0].split('/')[-1].split('_')[0]
         psr = Pulsar(p, t, ephem='DE438')
         psrs.append(psr)
 
+    ## Eliminate all pulsars that do not have a baseline greater than our specific cutoff.
     psrcut = []
     for psr in psrs:
         tmax = max(psr.toas)
@@ -105,6 +113,7 @@ else:
         if Tspan / 525600 / 60 > args.datacut_length:
             psrcut.append(psr)
             
+    ## Get the names of all of our remaining pulsars.
     psrcut_parfiles = []
     for psr in psrcut:
         for parfile in parfiles:
@@ -117,10 +126,12 @@ else:
             if psr.name in timfile:
                 psrcut_timfiles.append(timfile)
 
+## Write the pulsar names to the output file for record keeping.
 output_params.write("This dataset includes: \n")
 # import each pulsar into tempo2
 tempopsr = []
 i=0
+## Loop through the pulsars and make each a tempo2 object.
 for psr in psrcut:
     print(psr.name)
     output_params.write(psr.name + '\n')
@@ -129,21 +140,22 @@ for psr in psrcut:
     tempopsr.append(psr)
     i += 1
 
-# remove all noise from the pulsars
-# add in EFAC noise
+# remove all noise from the pulsars.
 for psr in tempopsr:
     LT.make_ideal(psr)
-    #LT.add_efac(psr, efac=1.0)
 
+## Inject fdm.
 if args.inject_fdm:
     print("Injecting Fuzzy Dark Matter!")
     for psr in tempopsr:
         LT.add_fdm(psr, args.fdm_A, args.fdm_f, args.fdm_phase_e, args.fdm_phase_p)
 
+# Inject a GWB.
 if args.inject_GWB:
     print("Injecting Gravitational Wave Background")
     LT.createGWB(tempopsr, Amp = args.GWB_A, gam = args.gamma)
 
+## Save the tim files. 
 print("Saving the tim files!")
 for psr in tempopsr:
     psr.savetim(args.timsavepath + psr.name + '-sim.tim')
